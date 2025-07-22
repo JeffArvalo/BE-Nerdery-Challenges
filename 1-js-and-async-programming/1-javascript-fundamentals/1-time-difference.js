67c093a3d0a16fe4edb6d52331814acfd0a905ec
Jeffer-Arevalo <jeffersonarevalo@ravn.co>
2025-07-22 11:02:28 -0600

    Solving comments in challenge 1

--- 1-js-and-async-programming/1-javascript-fundamentals/1-time-difference.js
+++ 1-js-and-async-programming/1-javascript-fundamentals/1-time-difference.js
@@ -16,17 +16,17 @@ timeDifference(7200, 3400); // Expected output: "01:03:20"
 
 */
 
-const formatTime = (num) => {
-  num = Math.floor(num);
-  return num < 10 ? `0${num}` : `${num}`;
+const formatNumToTwoDigits = (num) => {
+  const calculateTime = Math.floor(num).toString();
+  return calculateTime.padStart(2, "0");
 };
 
 const timeDifference = (a, b) => {
-  let diff = a > b ? a - b : b - a;
-  let hours = diff / 3600;
-  let mins = (diff % 3600) / 60;
-  let sec = diff % 60;
-  return `${formatTime(hours)}:${formatTime(mins)}:${formatTime(sec)}`;
+  const diff = Math.abs(a - b);
+  const hours = formatNumToTwoDigits(diff / 3600);
+  const mins = formatNumToTwoDigits((diff % 3600) / 60);
+  const sec = formatNumToTwoDigits(diff % 60);
+  return `${hours}:${mins}:${sec}`;
 };
 
 module.exports = timeDifference;
