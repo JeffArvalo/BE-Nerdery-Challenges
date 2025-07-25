de366bda3dcf7225d7115fc8f501c2fe3ec22f8d
JeffArvalo <jeffersonarevalo@ravn.co>
2025-07-25 17:39:51 -0600

    feat/1-ecommerce/solve-challenge-deep-clone

--- 3-typescript/2-custom-utility-types-and-generics/1-deep-clone.ts
+++ 3-typescript/2-custom-utility-types-and-generics/1-deep-clone.ts
@@ -12,3 +12,35 @@
  */
 
 //? implement the function  here
+import { readJsonFile } from "../1-ecommerce/utils/read-json.util";
+
+import {  Brand } from "../1-ecommerce/1-types";
+
+const deepClone = (obj: any): typeof obj => {
+  if (obj === null || typeof obj !== "object") {
+    return obj;
+  }
+
+  if (Array.isArray(obj)) {
+    return obj.map((item) => {
+      console.log(item);
+      return deepClone(item);
+    });
+  } else {
+    const cloneObj: any = {};
+    Object.keys(obj).forEach((key) => {
+      cloneObj[key] = deepClone(obj[key]);
+    });
+    return cloneObj;
+  }
+};
+
+readJsonFile<Brand>("../1-ecommerce/data/brands.json").then((brand) => {
+    const clonedProducts = deepClone(brand);
+    console.log(brand);
+    console.log(clonedProducts);
+    console.log("Original object:", typeof brand);
+    console.log("Cloned object:", typeof clonedProducts)
+    console.log(JSON.stringify(brand) === JSON.stringify(clonedProducts) ? "Same object" : "Different objects");
+    console.log("Are the original and cloned products the same object?", brand == clonedProducts);
+});
