9137866d16de1a8969e5734f793d08781a4ab75a
JeffArvalo <jeffersonarevalo@ravn.co>
2025-08-01 16:17:06 -0600

    feat/change-deep-clone-file

--- 3-typescript/2-custom-utility-types-and-generics/1-deep-clone.ts
+++ 3-typescript/2-custom-utility-types-and-generics/1-deep-clone.ts
@@ -14,33 +14,37 @@
 //? implement the function  here
 import { readJsonFile } from "../1-ecommerce/utils/read-json.util";
 
-import {  Brand } from "../1-ecommerce/1-types";
+import { Brand } from "../1-ecommerce/1-types";
 
-const deepClone = (obj: any): typeof obj => {
-  if (obj === null || typeof obj !== "object") {
+const deepClone = <T>(obj: T): T => {
+  if (!obj || typeof obj !== "object") {
     return obj;
   }
 
   if (Array.isArray(obj)) {
-    return obj.map((item) => {
-      console.log(item);
-      return deepClone(item);
-    });
+    return obj.map((item) => deepClone(item)) as unknown as T;
   } else {
     const cloneObj: any = {};
     Object.keys(obj).forEach((key) => {
-      cloneObj[key] = deepClone(obj[key]);
+      cloneObj[key] = deepClone((obj as any)[key]);
     });
     return cloneObj;
   }
 };
 
 readJsonFile<Brand>("../1-ecommerce/data/brands.json").then((brand) => {
-    const clonedProducts = deepClone(brand);
-    console.log(brand);
-    console.log(clonedProducts);
-    console.log("Original object:", typeof brand);
-    console.log("Cloned object:", typeof clonedProducts)
-    console.log(JSON.stringify(brand) === JSON.stringify(clonedProducts) ? "Same object" : "Different objects");
-    console.log("Are the original and cloned products the same object?", brand == clonedProducts);
+  const clonedProducts = deepClone(brand);
+  console.log(brand);
+  console.log(clonedProducts);
+  console.log("Original object:", typeof brand);
+  console.log("Cloned object:", typeof clonedProducts);
+  console.log(
+    JSON.stringify(brand) === JSON.stringify(clonedProducts)
+      ? "Same object"
+      : "Different objects",
+  );
+  console.log(
+    "Are the original and cloned products the same object?",
+    brand == clonedProducts,
+  );
 });
